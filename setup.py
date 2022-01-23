27e9a87cadf2e1e582df4a8a464cd6f25df9f6db
Ronilou Sumile <ronskido@gmail.com>
2022-01-23 17:08:37 +0800

    add python_build_files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+from setuptools import setup
+
+setup(
+    name = "Sample Project Name",
+    version = "0.0.1",
+    description= "insert short discreption here",
+    url = " ",
+    author = "Ronilou Sumile",
+    author_email= "ronskido@gmail.com",
+    license= "ECE",
+)
